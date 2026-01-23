6eae1b73fbe2f48ff2f85482e2da61e193aec0d5
elieEscal67 <elie.ferradji@escal.fr>
2026-01-23 14:57:47 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FaceSnapComponent } from './face-snap/face-snap.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+imports: [FaceSnapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
