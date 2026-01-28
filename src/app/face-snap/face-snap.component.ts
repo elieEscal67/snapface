d5dce4d70afbf0687a78853b0fcaf6f24e00ea34
elieEscal67 <elie.ferradji@escal.fr>
2026-01-28 16:32:09 +0100

    commit

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap';
-import { NgClass, NgStyle } from '@angular/common';
+import { DatePipe, NgClass, NgStyle, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-face-snap',
   standalone: true,
   imports: [NgStyle,
-            NgClass
+            NgClass,
+            UpperCasePipe,
+            DatePipe
   ],
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
