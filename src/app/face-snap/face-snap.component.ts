07f506f4339ef5de963eafad3667a91204f9aca3
elieEscal67 <elie.ferradji@escal.fr>
2026-01-28 15:43:20 +0100

    commit

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,42 +1,52 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap';
+import { NgClass, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-face-snap',
   standalone: true,
-  imports: [],
+  imports: [NgStyle,
+            NgClass
+  ],
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
 })
 export class FaceSnapComponent implements OnInit {
 
+  // --- @Input permet à ce composant enfant de recevoir un objet FaceSnap depuis le parent
   @Input() faceSnap!: FaceSnap;
 
-  snapButtonText!: string;
-  userHasSnapped!: boolean;
+  // --- Propriétés locales pour gérer l'état du bouton Snap
+  snapButtonText!: string;   // texte affiché sur le bouton selon l'état
+  userHasSnapped!: boolean;  // vrai si l'utilisateur a déjà cliqué sur Snap
 
   ngOnInit() {
+    // --- Initialisation de l'état local
+    // Angular appelle ngOnInit après avoir créé le composant et injecté @Input
     this.snapButtonText = 'Oh Snap!';
     this.userHasSnapped = false;
   }
 
+  // --- Méthode appelée lorsque l'utilisateur clique sur le bouton Snap
   onSnap(): void {
     if (this.userHasSnapped) {
-      this.unSnap();
+      this.unSnap(); // retire un snap si déjà cliqué
     } else {
-      this.snap();
+      this.snap();   // ajoute un snap sinon
     }
   }
 
+  // --- Retirer un snap
   unSnap(): void {
-    this.faceSnap.removeSnap();
+    this.faceSnap.removeSnap();   // modifie le modèle reçu du parent
     this.snapButtonText = 'Oh Snap!';
-    this.userHasSnapped = false;
+    this.userHasSnapped = false;  // met à jour l'état local
   }
 
+  // --- Ajouter un snap
   snap(): void {
-    this.faceSnap.addSnap();
+    this.faceSnap.addSnap();      // modifie le modèle reçu du parent
     this.snapButtonText = 'Oops, unSnap!';
-    this.userHasSnapped = true;
+    this.userHasSnapped = true;   // met à jour l'état local
   }
 }
