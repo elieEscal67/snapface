ba0a076cb9a708ed4bb1056af52b714893ebaddb
elieEscal67 <elie.ferradji@escal.fr>
2026-01-29 14:25:37 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
 import { FaceSnap } from './models/face-snap';
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   imports: [
+    HeaderComponent,
     FaceSnapListComponent
   ],
   templateUrl: './app.component.html',
