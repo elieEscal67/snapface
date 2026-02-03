8bf28ec47c164cb348bc0be99f6d6e81bca955cd
elieEscal67 <elie.ferradji@escal.fr>
2026-02-03 14:29:54 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,16 @@ import { FaceSnapComponent } from './face-snap/face-snap.component';
 import { FaceSnap } from './models/face-snap';
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
 import { HeaderComponent } from './header/header.component';
+import { LandingPageComponent } from "./landing-page/landing-page.component";
+
 
 @Component({
   selector: 'app-root',
   imports: [
     HeaderComponent,
-    FaceSnapListComponent
-  ],
+    RouterOutlet,
+    LandingPageComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
